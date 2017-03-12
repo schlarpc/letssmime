860aaa0531a936c69e750d4437de76f397ca83a1
Chaz Schlarp <schlarpc@gmail.com>
2017-03-11 23:58:19 -0800

    a different approach

--- letssmime/__main__.py
+++ letssmime/__main__.py
@@ -3,13 +3,8 @@ import sys
 import importlib
 
 if __name__ == '__main__' and __package__ is None:
-    if hasattr(sys, "_MEIPASS"):  # pyinstaller deployed
-        print(os.listdir(sys._MEIPASS))
-    print(__file__)
     package_path = os.path.dirname(os.path.abspath(__file__))
-    print(package_path)
     __package__ = os.path.basename(package_path)
-    print(__package__)
     loader = importlib.find_loader(__package__, os.path.join(package_path, '..'))
     module = loader.load_module(__package__)
     loader.exec_module(module)
