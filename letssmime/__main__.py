236aae287e0d3577e2deeab27092447b652803e2
Chaz Schlarp <schlarpc@gmail.com>
2017-03-11 23:39:44 -0800

    like a real detective

--- letssmime/__main__.py
+++ letssmime/__main__.py
@@ -4,7 +4,7 @@ import importlib
 
 if __name__ == '__main__' and __package__ is None:
     if hasattr(sys, "_MEIPASS"):  # pyinstaller deployed
-        print(sys._MEIPASS)
+        print(os.listdir(sys._MEIPASS))
     print(__file__)
     package_path = os.path.dirname(os.path.abspath(__file__))
     print(package_path)
