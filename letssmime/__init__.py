6e6696262287949746041203f2ada31dd1fd79e1
Chaz Schlarp <schlarpc@gmail.com>
2017-03-12 00:52:54 -0800

    More restructuring

--- letssmime/__init__.py
+++ letssmime/__init__.py
@@ -37,19 +37,20 @@ def complete_args_interactive(args):
         args.revocation_password = random_string(16)
     return args
 
-def generate_private_key(datastore):
+def generate_private_key(args, datastore):
     key = rsa.generate_private_key(
         public_exponent=65537,
         key_size=4096,
         backend=default_backend()
     )
-    datastore['private_key.pem'] = key.private_bytes(
+    key_pem = key.private_bytes(
         encoding=serialization.Encoding.PEM,
         format=serialization.PrivateFormat.TraditionalOpenSSL,
         encryption_algorithm=serialization.NoEncryption()
     )
+    return key_pem
 
-def send_application(datastore, args):
+def send_application(args, datastore):
     private_key = serialization.load_pem_private_key(
         datastore['private_key.pem'],
         password=None,
@@ -63,55 +64,42 @@ def send_application(datastore, args):
         spkac.create_spkac(private_key),
         args.revocation_password
     )
-    datastore['revocation_password.txt'] = args.revocation_password
+    return args.revocation_password
 
-def ask_for_collection_password(datastore):
+def ask_for_collection_password(args, datastore):
     collection_password = input('Collection password: ').strip()
-    datastore['collection_password.txt'] = collection_password
+    return collection_password
 
-def retrieve_certificate_chain(datastore):
+def retrieve_certificate_chain(args, datastore):
     collection_password = datastore['collection_password.txt'].decode('utf-8')
     certificate = comodo.retrieve_certificate_chain(collection_password)
-    datastore['certificate_chain.pem'] = certificate
+    return certificate
 
-def create_pkcs12_bundle(datastore):
+def create_pkcs12_bundle(args, datastore):
     pkcs12 = subprocess.check_output(['openssl', 'pkcs12', '-export',
         '-out', '-',
         '-inkey', datastore.path('private_key.pem'),
         '-in', datastore.path('certificate_chain.pem'),
         '-passout', 'pass:',
     ])
-    datastore['certificate_key_bundle.p12'] = pkcs12
-
-def run_letssmime():
-    args = get_args()
-    datastore = DataStore(args.email_address)
-
-    if 'private_key.pem' not in datastore:
-        print('Generating private key...')
-        generate_private_key(datastore)
-
-    if 'revocation_password.txt' not in datastore:
-        print('Sending application to Comodo...')
-        send_application(datastore, args)
-
-    if 'collection_password.txt' not in datastore:
-        print('An email has been sent to', args.email_address, 'containing a "collection password".')
-        ask_for_collection_password(datastore)
-
-    if 'certificate_chain.pem' not in datastore:
-        print('Retrieving certificate chain...')
-        retrieve_certificate_chain(datastore)
-
-    if 'certificate_key_bundle.p12' not in datastore:
-        print('Creating PKCS12 bundle...')
-        create_pkcs12_bundle(datastore)
-
-    print('Done! Your files are in', datastore.storage_path)
+    return pkcs12
 
 def main():
+    steps = [
+        ('private_key.pem', generate_private_key, 'Generating private key...'),
+        ('revocation_password.txt', send_application, 'Sending application to Comodo...'),
+        ('collection_password.txt', ask_for_collection_password, 'An email has been sent to your inbox containing a "collection password".'),
+        ('certificate_chain.pem', retrieve_certificate_chain, 'Retrieving certificate chain from Comodo...'),
+        ('certificate_key_bundle.p12', create_pkcs12_bundle, 'Creating PKCS12 bundle...'),
+    ]
     try:
-        run_letssmime()
+        args = get_args()
+        datastore = DataStore(args.email_address)
+        for desired_file, generating_func, status_message in steps:
+            if desired_file not in datastore:
+                print(status_message)
+                datastore[desired_file] = generating_func(args, datastore)
+        print('Done! Your files are located at', datastore.storage_path)
     except Exception as ex:
         print()
         print('Oops, something bad happened:')
@@ -119,5 +107,6 @@ def main():
         print()
     finally:
         if sys.stdin.isatty() and sys.stdout.isatty():
-            input('Press Enter to exit')
+            print('Press Enter to exit')
+            input()
 
