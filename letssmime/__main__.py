350ae042b1e75b593f38bc451ef7bec5d2078038
Chaz Schlarp <schlarpc@gmail.com>
2017-03-11 23:37:24 -0800

    mei is bae

--- letssmime/__main__.py
+++ letssmime/__main__.py
@@ -3,6 +3,8 @@ import sys
 import importlib
 
 if __name__ == '__main__' and __package__ is None:
+    if hasattr(sys, "_MEIPASS"):  # pyinstaller deployed
+        print(sys._MEIPASS)
     print(__file__)
     package_path = os.path.dirname(os.path.abspath(__file__))
     print(package_path)
