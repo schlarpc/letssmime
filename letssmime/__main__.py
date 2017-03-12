49cf1838f00513e9cced4e867901b40fef4cb8be
Chaz Schlarp <schlarpc@gmail.com>
2017-03-11 23:26:29 -0800

    Why is __main__ such a broken construct

--- letssmime/__main__.py
+++ letssmime/__main__.py
@@ -1,3 +1,14 @@
+import os
+import sys
+import importlib
+
+if __name__ == '__main__' and __package__ is None:
+    package_path = os.path.dirname(os.path.abspath(__file__))
+    __package__ = os.path.basename(package_path)
+    loader = importlib.find_loader(__package__, os.path.join(package_path, '..'))
+    module = loader.load_module(__package__)
+    loader.exec_module(module)
+
 if __name__ == '__main__':
     from . import main
     main()
